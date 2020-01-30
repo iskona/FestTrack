4d5bced0729d22ceaf517117930271b7bb1a228d
iskona <isony.d@gmail.com>
2020-01-29 16:19:14 -0800

    added latitude and longitude variables for searching nearby restaurants

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -8,16 +8,23 @@ $(document).ready(function () {
     $('#concertVenue').text(pageInfo._embedded.venues[0].name)
     $('#concertGenre').text(pageInfo.classifications[0].genre.name)
     $('#concertImage').attr("src", pageInfo.images[2].url)
-
+    console.log(pageInfo);
+    
     if (pageInfo.info) {
         $('#extraDetails').text(pageInfo.info + pageInfo.pleaseNote)
     } else {
         $('#extraDetails').text("No info provided.")
     }
+    
+    let lat = pageInfo._embedded.venues[0].location.latitude;
+    console.log(lat);
+    
+    let lon = pageInfo._embedded.venues[0].location.longitude;
+    console.log(lon);
 
     $.ajax({
         type: "GET",
-        url: "https://developers.zomato.com/api/v2.1/search?lat=47.6062&lon=-122.3321&radius=5000&sort=rating&count=4",
+        url: "https://developers.zomato.com/api/v2.1/search?lat="+lat+"&lon="+lon+"&radius=5000&sort=rating&count=4",
         async: true,
         dataType: "json",
         headers: { 'user-key': 'bad7750c8e32a21364c6344f9009cc75' },
